c994a25c58e7520ba3d3ad32507b15d682e8a581
RosaBen <benchabane.rosa@outlook.com>
2025-07-26 12:19:31 +0200

    added site on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "ttaforkv2.fly.dev" }
     config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "in-v3.mailjet.com",
